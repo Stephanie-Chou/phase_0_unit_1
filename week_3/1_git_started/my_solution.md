a8e52718fa8e01374a27657773393c516722edcb
Stephanie-Chou <stephanie.chou@outlook.com>
2014-05-27 11:09:34 -0700

    git and codecombat

--- week_3/1_git_started/my_solution.md
+++ week_3/1_git_started/my_solution.md
@@ -4,42 +4,51 @@ Define the following commands and describe how they work/what they do.
 
 #### add
 <!-- Your defnition here -->
+add changes to git
 
 #### branch
 <!-- Your defnition here -->
+a copy of the code to which separate commits can be made. branches can later be merged to the master branch. they can also be checked out
 
 #### checkout
 <!-- Your defnition here -->
-
+you can go back to the state of the file since the last commit. you just have to state which file you want.
 #### clone
 <!-- Your defnition here -->
-
+makes a copy of the repo locally
 #### commit
 <!-- Your defnition here -->
+commit the lastest change in your file.
 
 #### fetch
 <!-- Your defnition here -->
+fetch the lastest version of a branch
 
 #### log
 <!-- Your defnition here -->
-
+remembers all commit history in the order they were committed
 #### merge
 <!-- Your defnition here -->
+merge your branch to the master. This syncs the changes from the branch back into the master branch
 
 #### pull
 <!-- Your defnition here -->
+check for new changes on git since our last commit
 
 #### push
 <!-- Your defnition here -->
+tells git where to put commits when we are ready.
 
 #### reset
 <!-- Your defnition here -->
+unstage files
 
 #### rm
 <!-- Your defnition here -->
+removes files
 
 #### status
-
+check the status of git
 
 ## Release 4: Git Workflow
 
--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -5,12 +5,100 @@
 // command + / This will comment the line. 
  
  
+//rescue Mission
+this.moveDown();
+this.moveDown();
+this.moveRight();
+
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.moveRight();
+this.moveDown();
+this.attackNearbyEnemy();
+//grab the mushroom
+this.moveUp();
+this.moveRight();
+this.moveLeft();
+this.moveUp();
+
+this.attackNearbyEnemy();
+//drink me 
+ this.moveRight();
+this.attackNearbyEnemy();
+this.moveRight();
+this.moveDown();
+this.moveUp();
+this.moveRight();
+this.attackNearbyEnemy();
+ 
+ //taunt the guards
+ this.moveRight();
+this.bustDownDoor();
+
+// Delete the "//" in front of each line below.
+this.moveRight();
+this.say("Hey there!");
+this.moveLeft();
+this.moveLeft();
+this.say("Attack!");
+
+// Now get Phoebe to follow through the dungeon.
+this.moveRight();
+this.say("Follow me.");
+this.moveRight();
+this.moveRight();
+this.moveUp();
+this.moveRight();
+this.say("hey there");
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.moveRight();
  
  
  
- 
- 
- 
- 
+
+//t's a trap!
+this.moveDown();
+this.moveDown();
+this.say("hey there");
+this.moveUp();
+this.moveUp();
+this.say("attack!")
+
+//break the prison
+
+//taunt
+this.say("Follow me!");
+this.say("Hey");
+this.say("Over here!");
+this.say("Follow me");
+
+//cowardly taunt
+// Run your soldier out where the ogres can hear you.
+this.moveXY(46,22);
+// Say something!
+this.say("Hello!");
+// Then run back behind the arrow towers for safety.
+this.moveXY(71,12);
+
+
+//commanding followers
+this.say("Follow me!");
+this.moveXY(60,34);
+// Saying anything with "attack" will send nearby troops into
+// a battle mode.
+this.say("Attack everything!");
+
+//Mobile Artillery
+this.moveXY(47,43);
+this.attackXY(50,70);
+this.moveXY(47,39);
+this.attackXY(47,48);
+this.attackXY(68,55);
+this.moveXY(40,39);
+this.attackXY(52,47);
+
 // Reflection:
 // Write your reflection here.
